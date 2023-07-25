694a285660e2bac7acc0cba19aff723d90f6e5e1
Soron007 <mitrasouvik123@gmail.com>
2023-07-25 21:55:16 +0530

    listed banner products and defined type

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # withLoveByUJ MERNT ECOMMERCE APP
+
+# List Products
+
+1. Create Product Type.
+2. Create Products Array
+3. add Product Images
+4. Render Products
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
-import React from "react";
 import "./App.css";
+import { sampleProducts } from "./data";
 
 const App = () => {
   return (
     <div>
       <header>withLoveByUJ</header>
-      <main>Main Content</main>
+      <main>
+        <ul>
+          {sampleProducts.map((prod) => {
+            return (
+              <li key={prod?.slug}>
+                <img
+                  src={prod?.image}
+                  alt={prod?.name}
+                  className="product-image"
+                />
+                <h2>{prod?.name}</h2>
+                <p>${prod?.price}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
       <footer>All Rights Reserved</footer>
     </div>
   );
