60efe0d77f2d44841cf7af4b90f551cffb09c9d4
Soron007 <mitrasouvik123@gmail.com>
2023-07-27 20:18:19 +0530

    created dummy navbar using bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,42 @@
-import "./App.css";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import { sampleProducts } from "./data";
 
 const App = () => {
   return (
-    <div>
-      <header>withLoveByUJ</header>
+    <div className="d-flex flex-column">
+      <header>
+        <Navbar sticky="top" bg="light" data-bs-theme="light" expand="lg">
+          <Container>
+            <Navbar.Brand>withLoveByUJ</Navbar.Brand>
+          </Container>
+          <Nav className="me-3">
+            <Nav.Link href="/cart">Cart</Nav.Link>
+            <Nav.Link href="/signin">Sign In</Nav.Link>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {sampleProducts.map((prod) => {
-            return (
-              <li key={prod?.slug}>
-                <img
-                  src={prod?.image}
-                  alt={prod?.name}
-                  className="product-image"
-                />
-                <h2>{prod?.name}</h2>
-                <p>${prod?.price}</p>
-              </li>
-            );
-          })}
-        </ul>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((prod) => {
+              return (
+                <Col key={prod?.slug} sm={6} md={4} lg={3}>
+                  <img
+                    src={prod?.image}
+                    alt={prod?.name}
+                    className="product-image"
+                  />
+                  <h2>{prod?.name}</h2>
+                  <p>${prod?.price}</p>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
       </main>
-      <footer>All Rights Reserved</footer>
+      <div className="text-center">
+        <footer>All Rights Reserved</footer>
+      </div>
     </div>
   );
 };
