7da86da46b178522e26b5dc36e8bc9ef615c34ab
Soron007 <mitrasouvik123@gmail.com>
2023-08-02 12:30:23 +0530

    modified HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,27 +1,79 @@
+import { useReducer, useEffect } from "react";
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
-import { sampleProducts } from "../data";
-import { Link } from "react-router-dom";
+// import { sampleProducts } from "../data";
+import { MessageBox } from "../components/MessageBox";
 
-const HomePage = () => {
-  return (
+import { Product } from "../types/Product";
+import { ApiError } from "../types/ApiError";
+import { getError } from "../utils";
+import { LoadingBox } from "../components/LoadingBox";
+import ProductItem from "../components/ProductItem";
+
+type State = {
+  products: Product[];
+  loading: boolean;
+  error: string;
+};
+
+type Action =
+  | { type: "FETCH_REQUEST" }
+  | {
+      type: "FETCH_SUCCESS";
+      payload: Product[];
+    }
+  | { type: "FETCH_FAIL"; payload: string };
+
+const initialState: State = {
+  products: [],
+  loading: true,
+  error: "",
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+  }
+};
+
+export const HomePage = () => {
+  const [{ loading, error, products }, dispatch] = useReducer<
+    React.Reducer<State, Action>
+  >(reducer, initialState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (error) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(error as ApiError) });
+      }
+    };
+    fetchData();
+  }, []);
+
+  return loading ? (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
     <Row>
-      {sampleProducts.map((prod) => {
+      {products.map((prod) => {
         return (
           <Col key={prod?.slug} sm={6} md={4} lg={3}>
-            <Link to={"/product/" + prod.slug}>
-              <img
-                src={prod?.image}
-                alt={prod?.name}
-                className="product-image"
-              />
-              <h2>{prod?.name}</h2>
-              <p>${prod?.price}</p>
-            </Link>
+            <ProductItem product={prod} />
           </Col>
         );
       })}
     </Row>
   );
 };
-
-export default HomePage;
