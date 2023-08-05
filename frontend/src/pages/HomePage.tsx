6a89be39f08e83a9c6ef822ee1a9d3dadea14a07
Soron007 <mitrasouvik123@gmail.com>
2023-08-05 22:52:00 +0530

    shortened code and implemented custom hook

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,73 +1,25 @@
-import { useReducer, useEffect } from "react";
-import axios from "axios";
 import { Row, Col } from "react-bootstrap";
-// import { sampleProducts } from "../data";
 import { MessageBox } from "../components/MessageBox";
-
-import { Product } from "../types/Product";
-import { ApiError } from "../types/ApiError";
-import { getError } from "../utils";
 import { LoadingBox } from "../components/LoadingBox";
 import ProductItem from "../components/ProductItem";
+import { Helmet } from "react-helmet-async";
+import { useGetProductsQuery } from "../hooks/productHooks";
+import { getError } from "../utils";
+import { ApiError } from "../types/ApiError";
 
-type State = {
-  products: Product[];
-  loading: boolean;
-  error: string;
-};
+export default function HomePage() {
+  const { data, isLoading, error } = useGetProductsQuery();
 
-type Action =
-  | { type: "FETCH_REQUEST" }
-  | {
-      type: "FETCH_SUCCESS";
-      payload: Product[];
-    }
-  | { type: "FETCH_FAIL"; payload: string };
-
-const initialState: State = {
-  products: [],
-  loading: true,
-  error: "",
-};
-
-const reducer = (state: State, action: Action) => {
-  switch (action.type) {
-    case "FETCH_REQUEST":
-      return { ...state, loading: true };
-
-    case "FETCH_SUCCESS":
-      return { ...state, products: action.payload, loading: false };
-
-    case "FETCH_FAIL":
-      return { ...state, loading: false, error: action.payload };
-  }
-};
-
-export const HomePage = () => {
-  const [{ loading, error, products }, dispatch] = useReducer<
-    React.Reducer<State, Action>
-  >(reducer, initialState);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      dispatch({ type: "FETCH_REQUEST" });
-      try {
-        const result = await axios.get("/api/products");
-        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
-      } catch (error) {
-        dispatch({ type: "FETCH_FAIL", payload: getError(error as ApiError) });
-      }
-    };
-    fetchData();
-  }, []);
-
-  return loading ? (
+  return isLoading ? (
     <LoadingBox />
   ) : error ? (
-    <MessageBox variant="danger">{error}</MessageBox>
+    <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
   ) : (
     <Row>
-      {products.map((prod) => {
+      <Helmet>
+        <title>With❤️ByUJ</title>
+      </Helmet>
+      {data!.map((prod) => {
         return (
           <Col key={prod?.slug} sm={6} md={4} lg={3}>
             <ProductItem product={prod} />
@@ -76,4 +28,4 @@ export const HomePage = () => {
       })}
     </Row>
   );
-};
+}
