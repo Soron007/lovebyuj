223c04ab0bb5894fdce018712e6b6c54e309bc70
Soron007 <mitrasouvik123@gmail.com>
2023-08-10 13:13:11 +0530

    added cors and defined api for single product

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { sampleProducts } from "./data";
 
 const app = express();
 
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173"],
+  })
+);
+
 app.get("/api/products", (req: Request, res: Response) => {
   res.json(sampleProducts);
+});
+
+app.get("/api/products/:slug", (req: Request, res: Response) => {
+  res.json(sampleProducts.find((x) => x.slug === req.params.slug));
 });
 
 const PORT = 4000;
