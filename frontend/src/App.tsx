bfe7adc34fe49fce97163972cef15fa29b5db1a3
Soron007 <mitrasouvik123@gmail.com>
2023-08-11 13:12:51 +0530

    used context api for state mgmt for theme change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,34 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { useContext, useEffect } from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import { Store } from "./Store";
 
 const App = () => {
+  const {
+    state: { mode },
+    dispatch,
+  } = useContext(Store);
+
+  useEffect(() => {
+    document.body.setAttribute("data-bs-theme", mode);
+  }, [mode]);
+
+  const switchModeHandler = () => {
+    dispatch({ type: "SWITCH_MODE" });
+  };
+
   return (
     <div className="d-flex flex-column">
       <header>
-        <Navbar sticky="top" bg="dark" data-bs-theme="dark" expand="lg">
+        <Navbar sticky="top" expand="lg">
           <Container>
             <Navbar.Brand>with❤️ByUJ</Navbar.Brand>
           </Container>
 
           <Nav className="me-3">
+            <Button variant={mode} onClick={switchModeHandler}>
+              <i className={mode === "light" ? "fa fa-sun" : "fa fa-moon"}></i>
+            </Button>
             <Nav.Link href="/cart">Cart</Nav.Link>
             <Nav.Link href="/signin">Sign In</Nav.Link>
           </Nav>
